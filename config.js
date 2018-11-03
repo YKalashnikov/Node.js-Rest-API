d6c69af0841ba490f79f4641db8302a457975cd6
YKalashnikov <uk2709@gmail.com>
2018-11-03 04:28:14 -0700

    typo

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 3000,
     URL: process.env.BASE_URL || 'http://localhost:3000',
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://users:12345678U@ds151293.mlab.com:51293/customer_api'
-}
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://users:users123@ds151293.mlab.com:51293/users'
+};
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const restify = requier('restify');
-const mongoose = require('mpngoose');
-const config = requre('./config');
+const restify = require('restify');
+const mongoose = require('mongoose');
+const config = require('./config');
 
 const server = restify.createServer();
+
 server.use(restify.plugins.bodyParser());
 
-server.listen(config.PORT, ()=> {
-    mongoose.connect(config.MONGOOSE_URL,
+server.listen(config.PORT, () => {
+    mongoose.connect(config.MONGODB_URI,
     { useNewUrlParser: true})
 });
 
 const db = mongoose.connection;
 
-db.on('error', err => console.log(err))
+db.on('error', err => console.log(err));
 
 db.once('open', () => {
     require('./routes/customers')(server);
