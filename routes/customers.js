dec8ddb8acd1ea77584c27d406b2f60da7bfe279
YKalashnikov <uk2709@gmail.com>
2018-11-04 23:30:12 -0800

    add update

--- routes/customers.js
+++ routes/customers.js
@@ -2,6 +2,7 @@ const errors = require('restify-errors');
 const Customer = require('../models/Customer');
 
 module.exports = server => {
+    // Getting all customer in db
     server.get('/customers', async (req, res, next) => {
         
         try {
@@ -11,7 +12,59 @@ module.exports = server => {
         catch (err) {
             return next(new errors.InvalidContentError(err));
         }
-        
-    });
+    })
+        //Get one customer
+       server.get('/customers/:id', async (req, res, next ) => {
+           const id = req.params.id;
+
+           try {
+               const customer= await Customer.findById(id);
+               res.send(customer);
+           }
+           catch(err) {
+               return next(new errors.ResourceNotFoundError(
+                   `There is no custome rwith the id ${req.params.id}`
+               ));
+           }
+       })
+          
+       //Adding a customer
+    server.post('/customers', async(req, res, next) =>{
+        if(!req.is('application/json')) {
+            return next(new errors.InvalidContentError("Expects Application/json"))
+        }
+
+        const { name, email, balance } = req.body;
+        const customer = new Customer({
+            name,
+            email,
+            balance
+        })
+        try {
+            const Customer = await customer.save();
+            res.send(201);
+            next();
+        } catch(err) {
+             return next(new errors.InvalidContentError(err.message));
+        }
+    })
+
+    //Updating customers
+      server.put('/customers/:id', async (req, res, next) => {
+          if(!req.is('application/json')) {
+              return res.send(new errors.InvalidContentError('Expect application/json'))
+          }
+          const id = req.params._id;
+          try {
+              const customer = await Customer.findOneAndUpdate({id}, req.body)
+              res.send(200);
+              next();
+           } catch(err) {
+                return next(new errors.ResourceNotFoundError(
+                    `There is no customer with the id ${id}`))
+            }
+      })
     
-};
+}
+      
+
